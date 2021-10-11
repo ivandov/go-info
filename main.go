f5602e7d2fd7c087849ff3112ac1c608a871ec06
Ivan Dovgan <ivandov@us.ibm.com>
2021-10-11 19:00:34 -0400

    Simplified, no HTTP API

--- main.go
+++ main.go
@@ -2,21 +2,20 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"log"
-	"net/http"
 	"runtime"
 )
 
 func main() {
-	goInfo := fmt.Sprintf("go version: %q, GOOS: %q, GOARCH: %q\n",
+	goInfo := fmt.Sprintf("go version: %q, GOOS: %q, GOARCH: %q",
 		runtime.Version(), runtime.GOOS, runtime.GOARCH)
 
-	goHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, goInfo)
-	}
+	fmt.Println(goInfo)
 
-	http.HandleFunc("/go", goHandler)
-	log.Println("Listening for requests at http://localhost:8000/go")
-	log.Fatal(http.ListenAndServe("0.0.0.0:8000", nil))
+	// goHandler := func(w http.ResponseWriter, req *http.Request) {
+	// 	io.WriteString(w, goInfo)
+	// }
+
+	// http.HandleFunc("/go", goHandler)
+	// log.Println("Listening for requests at http://localhost:8000/go")
+	// log.Fatal(http.ListenAndServe("0.0.0.0:8000", nil))
 }
